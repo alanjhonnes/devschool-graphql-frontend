7e7857e75461a667b6ed6b1857462572b6745df1
Alan Jhonnes <alanjhonnes@gmail.com>
2021-08-18 21:17:50 -0300

    feat: codegen and examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Apollo, gql, QueryRef } from 'apollo-angular';
+import { AddUserGQL, ExampleQueryGQL, ExampleQueryQuery } from 'src/shared/graphql';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devschool-graphql-frontend';
+  query: QueryRef<ExampleQueryQuery, any>;
+
+  constructor(
+    private apollo: Apollo,
+    private usersQuery: ExampleQueryGQL,
+    private addUserGQL: AddUserGQL) {
+    this.query = usersQuery.watch()
+
+    this.query.valueChanges.subscribe({
+      next: (next) => {
+        console.log(next);
+      }
+    })
+  }
+
+  fetchUsers() {
+    this.query.refetch();
+  }
+
+  addUser() {
+    this.addUserGQL.mutate({
+      addUserData: {
+        name: '345345',
+      }
+    })
+    .subscribe({
+      next: (next) => {
+        console.log(next.data?.addUser.id);
+      }
+    })
+  }
 }
---
+++ src/shared/graphql.ts
@@ -0,0 +1,110 @@
+import { gql } from 'apollo-angular';
+import { Injectable } from '@angular/core';
+import * as Apollo from 'apollo-angular';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  addUser: User;
+};
+
+
+export type MutationAddUserArgs = {
+  data: UserInput;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  users: Array<User>;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  friends: Array<User>;
+  firstName: Scalars['String'];
+};
+
+export type UserInput = {
+  name: Scalars['String'];
+};
+
+export type AddUserMutationVariables = Exact<{
+  addUserData: UserInput;
+}>;
+
+
+export type AddUserMutation = { __typename?: 'Mutation', addUser: { __typename?: 'User', id: string, name: string } };
+
+export type ExampleQueryQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type ExampleQueryQuery = { __typename?: 'Query', users: Array<{ __typename?: 'User', id: string, name: string, firstName: string, friends: Array<{ __typename?: 'User', id: string, name: string, firstName: string, friends: Array<{ __typename?: 'User', id: string, friends: Array<{ __typename?: 'User', name: string, friends: Array<{ __typename?: 'User', name: string }> }> }> }> }> };
+
+export const AddUserDocument = gql`
+    mutation AddUser($addUserData: UserInput!) {
+  addUser(data: $addUserData) {
+    id
+    name
+  }
+}
+    `;
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class AddUserGQL extends Apollo.Mutation<AddUserMutation, AddUserMutationVariables> {
+    document = AddUserDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
+export const ExampleQueryDocument = gql`
+    query ExampleQuery {
+  users {
+    id
+    name
+    firstName
+    friends {
+      id
+      name
+      firstName
+      friends {
+        id
+        friends {
+          name
+          friends {
+            name
+          }
+        }
+      }
+    }
+  }
+}
+    `;
+
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class ExampleQueryGQL extends Apollo.Query<ExampleQueryQuery, ExampleQueryQueryVariables> {
+    document = ExampleQueryDocument;
+    
+    constructor(apollo: Apollo.Apollo) {
+      super(apollo);
+    }
+  }
