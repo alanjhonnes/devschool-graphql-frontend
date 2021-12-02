aece323623bb1ad4874fe1160b3d1356c4f233ed
Alan Jhonnes <alanjhonnes@gmail.com>
2021-12-01 21:29:28 -0300

    examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devschool-graphql-frontend';
+  constructor(apollo: Apollo) {
+    const watchQuery = apollo.watchQuery({
+      query: gql`query ExampleQuery {
+        users {
+          id
+          name
+        }
+      }
+      `,
+      fetchPolicy: 'network-only',
+
+    });
+    watchQuery.valueChanges.subscribe(result => {
+      console.log(result);
+    })
+
+    apollo.mutate({
+      mutation: gql`
+        mutation addUser($data: UserInput!) {
+          addUser(data: $data) {
+            id
+            name
+          }
+        }
+      `,
+      variables: {
+        data: {
+          name: "Ravi"
+        }
+      }
+    }).subscribe(result => {
+      console.log(result);
+    })
+
+  }
 }
